f5b0b338ac450452b72083c65eaa875cccb0ba59
Helen Liu <helenhliu01@gmail.com>
2024-01-30 15:42:42 -0600

    refactoring

--- src/timeSlots.js
+++ src/timeSlots.js
@@ -1,5 +1,5 @@
 import {SCHOOLS, TIMES} from './configs.js';
-import {populateStudents, RenderStudent} from './students';
+import {RenderStudent} from './students';
 import {useDroppable} from '@dnd-kit/core';
 
 function schools(times){
@@ -12,49 +12,33 @@ function schools(times){
     }  
 }
 
-function RenderSchools({schools}){ 
-  //console.log(schools);
-  //for(const sch in schools){
-  //  console.log(schools[sch].studentList);
-  //  schools[sch].studentList.map((student) => console.log(student));
-  //}
-  const {isOver, setNodeRef} = useDroppable({ //drop not seeming to work rn
-    id: schools[0], //do i need to change id?seems to not be unique rn bci have one componentn rendering multiple schools
-  });
+function RenderSchool({school}){
+  const {isOver, setNodeRef} = useDroppable({ 
+    id: school, 
+  }); 
   const style = { 
     color: isOver ? 'green' : undefined,
   };
-    return schools.map((school) => (
+    return (
         <div className="school" ref={setNodeRef} style={style} key={school.name} id={school.name}>
             <p className="schoolNameText" >{school.name}</p>
             {school.studentList.map((student) => <RenderStudent student={student} />)}
         </ div>
-    ));
+    );
 }
 
-function TimeSlot({time}){
-    if(time.orange){
-      return (
-        <div className="timeSlot orangeTimeSlot" key={time.id}>
-        <h4 >{time.timeName}</h4>
-        <RenderSchools schools={time.schools} />
-        </div>)
-    }
-    else{
-      return (
-        <div className="timeSlot" key={time.id}>
-        <h4 >{time.timeName}</h4>
-        <RenderSchools schools={time.schools} />
-        </div>)
-    }
-  }
-  
 function AllTimeSlots(){ 
   schools(TIMES);
 
-  return TIMES.map((time) => (
-    <TimeSlot time={time} />
-  ));
+  return TIMES.map((time) => 
+      (<div className={`timeSlot  ${time.orange ? "orangeTimeSlot" : ""}`} key={time.id}>
+      <h4 >{time.timeName}</h4>
+      {time.schools.map((school) => 
+        (<RenderSchool school={school} />)
+      )}
+      </div>)
+);
+
 }
 
 export default AllTimeSlots;
