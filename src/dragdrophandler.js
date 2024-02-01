9f0641448f653bff0fef60ad4abc8fc4c085c4f7
Helen Liu <helenhliu01@gmail.com>
2024-01-31 21:18:20 -0600

    supports unsorted students

--- src/dragdrophandler.js
+++ src/dragdrophandler.js
@@ -9,12 +9,11 @@ function handleDragEnd(event) {
 }
   
 function dataTransfer({student, dest}){ 
-    var src = SCHOOLS.find(school => school.name === student.schoolName); //need to handle if comnig from unsorted
-    console.log(student);
-    console.log(dest);
+    var src = SCHOOLS.find(school => school.name === student.schoolName)
+    if(src === undefined){src = SCHOOLS.find(school => school.name === "Unsorted");}
     if(validDrop({student, dest})){
         src.studentList.splice(src.studentList.indexOf(student), 1);
-        student.schoolName = dest.name;
+        student.schoolName = dest.name=== "Unsorted" ? undefined : dest.name;
         dest.studentList.push(student);
     }
     else{
